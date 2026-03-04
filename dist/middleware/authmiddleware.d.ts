1d97b760e2a1d0b62899d5f007c6cf3812ba5812
Ratnesh Kumar <ratneshrt19@gmail.com>
2026-03-05 03:40:29 +0530

    added test backend testing using jest

--- dist/middleware/authmiddleware.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-export interface AuthRequest extends Request {
-    userId?: string;
-}
-export declare const authMiddleware: (req: AuthRequest, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=authmiddleware.d.ts.map
